ca1adecde6c6a352995e43206f8428842ba47b3f
Jim Lee <jimlee2002@outlook.com>
2024-02-13 22:04:09 +0800

    Refactor sub-info-node.js to improve readability and maintainability

--- sub-store/scripts/sub-info-node.js
+++ sub-store/scripts/sub-info-node.js
@@ -1,26 +1,31 @@
 async function operator(proxies = [], targetPlatform, env) {
-    const { parseFlowHeaders, getFlowHeaders, flowTransfer } = flowUtils
-    const {
-      expires,
-      total,
-      usage: { upload, download },
-    } = parseFlowHeaders(await getFlowHeaders(env.source[proxies[0].subName].url))
-    const date = expires ? new Date(expires * 1000).toISOString().split('T')[0] : '';
-  
-    const prefixName = env.source[proxies[0].subName].displayName || env.source[proxies[0].subName].name
-    const current = upload + download
-    const currT = flowTransfer(Math.abs(current))
-    currT.value = current < 0 ? '-' + currT.value : currT.value
-    const totalT = flowTransfer(total)
-  
-    proxies.unshift({
-      type: 'ss',
-      server: '127.0.0.1',
-      port: 8080,
-      cipher: 'aes-128-gcm',
-      password: 'password',
-      name: `ℹ️ ${prefixName}: ${currT.value} ${currT.unit} / ${totalT.value} ${totalT.unit} ${date}`,
-    })
-    return proxies
-  }
-  
+  const { parseFlowHeaders, getFlowHeaders, flowTransfer } = flowUtils;
+  const {
+    expires,
+    total,
+    usage: { upload, download },
+  } = parseFlowHeaders(
+    await getFlowHeaders(env.source[proxies[0].subName].url)
+  );
+  const date = expires
+    ? new Date(expires * 1000).toISOString().split("T")[0]
+    : "";
+
+  const prefixName =
+    env.source[proxies[0].subName].displayName ||
+    env.source[proxies[0].subName].name;
+  const current = upload + download;
+  const currT = flowTransfer(Math.abs(current));
+  currT.value = current < 0 ? "-" + currT.value : currT.value;
+  const totalT = flowTransfer(total);
+
+  proxies.unshift({
+    type: "ss",
+    server: "127.0.0.1",
+    port: 8080,
+    cipher: "aes-128-gcm",
+    password: "password",
+    name: `ℹ️ ${prefixName}: ${currT.value} ${currT.unit} / ${totalT.value} ${totalT.unit} ${date}`,
+  });
+  return proxies;
+}
